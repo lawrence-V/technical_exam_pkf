20be31afd48353d30267c892fa4f967d80151612
Lawrence <lawrencevaldez027@gmail.com>
2026-03-16 22:58:14 +0800

    Install nuxt UI and pinia

---
+++ app/pages/index.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { PricingPlanProps } from '@nuxt/ui'
+
+const plans = ref<PricingPlanProps[]>([
+  {
+    title: 'Solo',
+    description: 'Tailored for indie hackers.',
+    price: '$249',
+    features: [
+      'One developer',
+      'Lifetime access'
+    ],
+    button: {
+      label: 'Buy now'
+    }
+  },
+  {
+    title: 'Startup',
+    description: 'Best suited for small teams.',
+    price: '$499',
+    features: [
+      'Up to 5 developers',
+      'Everything in Solo'
+    ],
+    button: {
+      label: 'Buy now'
+    }
+  },
+  {
+    title: 'Organization',
+    description: 'Ideal for larger teams and organizations.',
+    price: '$999',
+    features: [
+      'Up to 20 developers',
+      'Everything in Startup'
+    ],
+    button: {
+      label: 'Buy now'
+    }
+  }
+])
+</script>
+
+<template>
+  <UPricingPlans :plans="plans" />
+</template>
